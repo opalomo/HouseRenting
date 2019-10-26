48e0384b9557ce38558e867c269ac93c50a885b3
Oscar <palomoscar@gmail.com>
2019-10-26 13:39:22 +0200

    Added folder hyeranchy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/Pages/home/home.component';
+import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
+import { LoginComponent } from './Components/Pages/login/login.component';
+import { RegisterComponent } from './Components/Pages/register/register.component';
+import { ProfileComponent } from './Components/Pages/profile/profile.component';
+import { CatalogComponent } from './Components/Pages/catalog/catalog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavBarComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    CatalogComponent
   ],
   imports: [
     BrowserModule,

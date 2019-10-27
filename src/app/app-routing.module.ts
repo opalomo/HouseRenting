cad7d69ff76cafa7aae194c3281889266f07d1f8
Oscar <palomoscar@gmail.com>
2019-10-27 17:58:26 +0100

    Basic routing logic added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './Components/Pages/login/login.component';
+import { RegisterComponent } from './Components/Pages/register/register.component';
+import { CatalogComponent } from './Components/Pages/catalog/catalog.component';
+import { ProfileComponent } from './Components/Pages/profile/profile.component';
+import { HomeComponent } from './Components/Pages/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'user/login', component : LoginComponent
+  },
+  {
+    path: 'user/register', component: RegisterComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [LoginComponent, RegisterComponent, CatalogComponent, ProfileComponent, HomeComponent ];
+
+// Una buena practica para que app.module.ts no se llene de imports
+// es crear un array con todos los imports necesarios para que no se pete la otra clase
+// en cualquier caso, como no va a ser una ruta por componente este metoodo puede tener sus
+// partes flacas
+// Como en este caso he creado una carpeta llamada "Pages" dentro de components, todo lo que este ahui dentro
+// van a ser paginas que enrutar, por lo que el array contendra todos los componentes de dicha carpeta
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './Components/Pages/home/home.component';
+
 import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
-import { LoginComponent } from './Components/Pages/login/login.component';
-import { RegisterComponent } from './Components/Pages/register/register.component';
-import { ProfileComponent } from './Components/Pages/profile/profile.component';
-import { CatalogComponent } from './Components/Pages/catalog/catalog.component';
 import { FooterComponent } from './Components/footer/footer.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
+    routingComponents, // includes the components from "//Components/Pages/" folder
     NavBarComponent,
-    LoginComponent,
-    RegisterComponent,
-    ProfileComponent,
-    CatalogComponent,
     FooterComponent
   ],
   imports: [
